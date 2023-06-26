4da7ec24f018fa7656d14dc4c1369286056d4a45
4ndy-C <72530972+Cookie-And-Milk@users.noreply.github.com>
2023-06-26 13:54:10 +0800

    new field added
    - regex for user name
    - length for the phone number
    - regex for the phone number

--- BlazorDemo/BlazorDemo/Shared/People.cs
+++ BlazorDemo/BlazorDemo/Shared/People.cs
@@ -4,10 +4,18 @@ namespace BlazorDemo.Shared
 {
     public class People
     {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]{2,}$", ErrorMessage = "Name cannot contain numbers or special characters. ")]
         public string? Name { get; set; }
         public int? Age { get; set; }
+        [Required]
         [EmailAddress]
         public string? Email { get; set; }
+        [Required]
+        [MaxLength(14, ErrorMessage = "Exceeding maximum length. ")]
+        [RegularExpression(@"^\+[0-9]+", ErrorMessage = "Phone number cannot contain special characters or letters, apart from +. ")]
+        public string? PhoneNum { get; set; }
+
     }
 }
 
